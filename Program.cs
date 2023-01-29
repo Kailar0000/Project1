dc9d953536b3bdf17c89d39520074a44eaf5542d
Kailar0000 <kailar0000@gmail.com>
2023-01-29 16:46:00 +0300

    Program

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿string? test(string[] s)
+{
+    string? a="TEST";
+    for(int i=0; i<s.Length; i++)
+    {
+        if(s[i].Length<4)
+        {
+            if(a!="TEST")
+            a=a+", "+s[i];
+            else
+            a=s[i];
+        }
+    }
+    return a;
+}
+
+void PrintMatrix(string[] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+    Console.WriteLine(matrix[i]);
+    }
+}
+
+Console.WriteLine("Ведите элементы массива через , :");
+string[] s = Console.ReadLine().Split(", ");
+string? a=test(s);
+string[] b=a.Split(", ");
+PrintMatrix(b);
